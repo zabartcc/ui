67f8f1ec657555178c9017402fa3a0fb394028b0
Austin Robison <austin@arobison.net>
2020-10-24 22:40:48 -0700

    remove scoping of materialize styles, no more !importants!

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'materialize-css';
+import 'materialize-css/sass/materialize.scss';
 
 createApp(App).use(store).use(router).mount('#app');
