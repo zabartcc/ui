cf69201e9fc8eac5abbf781815abb7b86c968995
Austin Robison <austin@arobison.net>
2020-10-29 01:34:24 -0700

    spaces -> tabs

--- src/mixins/ControllerMixin.js
+++ src/mixins/ControllerMixin.js
@@ -1,9 +1,9 @@
 import axios from '@/helpers/axios.js';
 
 export const ControllerMixin = {
-    methods: {
-        getControllersMixin() {
-            return axios.get('/controller').then(response => response.data).catch((err) => { console.log(err); });
-        }
-    }
+	methods: {
+		getControllersMixin() {
+			return axios.get('/controller').then(response => response.data).catch((err) => { console.log(err); });
+		}
+	}
 };
--- src/mixins/SidebarMixin.js
+++ src/mixins/SidebarMixin.js
@@ -1,12 +1,12 @@
 import axios from '@/helpers/axios.js';
 
 export const SidebarMixin = {
-    methods: {
-        getPilotsOnline() {
-            return axios.get('/online/pilots').then(response => response.data).catch((err) => { console.log(err);  });
-        },
-        getAtcOnline() {
-            return axios.get('/online/atc').then(response => response.data).catch((err) => { console.log(err); });
-        }
-    }
+	methods: {
+		getPilotsOnline() {
+			return axios.get('/online/pilots').then(response => response.data).catch((err) => { console.log(err);  });
+		},
+		getAtcOnline() {
+			return axios.get('/online/atc').then(response => response.data).catch((err) => { console.log(err); });
+		}
+	}
 };
