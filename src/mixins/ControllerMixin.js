5b08cb222fe114fe86c72549608a8f24cdad58e5
Austin Robison <austin@arobison.net>
2020-11-04 23:11:13 -0700

    wip edit controller

--- src/mixins/ControllerMixin.js
+++ src/mixins/ControllerMixin.js
@@ -6,7 +6,7 @@ export const ControllerMixin = {
 			return axios.get('/controller').then(response => response.data).catch((err) => console.log(err));
 		},
 		getControllerMixin(cid) {
-			return axios.get(`/controller/find/${cid}`).then(response => response.data).catch((err) => console.log(err));
+			return axios.get(`/controller/${cid}`).then(response => response.data).catch((err) => console.log(err));
 		},
 		getUsedOiMixin() {
 			return axios.get(`/controller/oi`).then(response => response.data).catch((err) => console.log(err));
--- src/views/admin/controllers/Edit.vue
+++ src/views/admin/controllers/Edit.vue
@@ -2,98 +2,175 @@
 	<div v-if=controller class="card">
 		<div class="card-content">
 			<span class="card-title">Edit Controller - {{controller.fname}} {{controller.lname}}</span>
-			<div class="row">
-				<div class="input-field col s6">
-					<input id="cid" type="text" :value="controller.cid" disabled>
-					<label for="cid" class="active">Controller ID</label>
-				</div>
-				<div class="input-field col s6">
-					<input id="rating" type="text" :value="controller.ratingLong" disabled>
-					<label for="rating" class="active">Rating</label>
-				</div>
-				<div class="input-field col s6">
-					<input id="first_name" type="text" :value="controller.fname">
-					<label for="first_name" class="active">First Name</label>
-				</div>
-				<div class="input-field col s6">
-					<input id="last_name" type="text" :value="controller.lname">
-					<label for="last_name" class="active">Last Name</label>
-				</div>
-				<div class="input-field col s6">
-					<input id="email" type="email" :value="controller.email">
-					<label for="email" class="active">Email</label>
-				</div>
-				<div class="input-field col s6 input_oi">
-					<input id="oi" type="text" v-model="oi" @input="checkOi($event)" maxlength="2">
-					<label for="oi" class="active">Operating Initials</label>
-					<div class="side_oi right">
-						<i v-if="this.available == true" class="material-icons green-text">check_circle</i>
-						<i v-else class="material-icons red-text">remove_circle</i>
+			<form id="update_controller" @submit.prevent=updateController>
+				<div class="row">
+					<div class="input-field col s6">
+						<input id="cid" type="text" :value="controller.cid" disabled>
+						<label for="cid" class="active">Controller ID</label>
+					</div>
+					<div class="input-field col s6">
+						<input id="rating" type="text" :value="controller.ratingLong" disabled>
+						<label for="rating" class="active">Rating</label>
+					</div>
+					<div class="input-field col s6">
+						<input id="first_name" type="text" v-model="form.fname">
+						<label for="first_name" class="active">First Name</label>
+					</div>
+					<div class="input-field col s6">
+						<input id="last_name" type="text" v-model="form.lname">
+						<label for="last_name" class="active">Last Name</label>
+					</div>
+					<div class="input-field col s6">
+						<input id="email" type="email" v-model="form.email">
+						<label for="email" class="active">Email</label>
+					</div>
+					<div class="input-field col s6 input_oi">
+						<input id="oi" type="text" v-model="form.oi" @input="checkOi($event)" maxlength="2">
+						<label for="oi" class="active">Operating Initials</label>
+						<div class="side_oi right">
+							<i v-if="this.oiAvail == true" class="material-icons green-text">check_circle</i>
+							<i v-else class="material-icons red-text">remove_circle</i>
+						</div>
+					</div>
+					<div class="input-field col s12">
+						<label for="certs" class="active">Certifications</label>
+						<div id="certs_container">
+							<span 
+								id="zab" 
+								:class="{active: form.certs.zab}" 
+								class="cert cert_center" 
+								@click="toggleCert">Albuquerque Center</span>
+							<span 
+								id="p50app" 
+								:class="{active: form.certs.p50app}" 
+								class="cert cert_major" 
+								@click="toggleCert">Major Approach</span>
+							<span 
+								id="p50twr" 
+								:class="{active: form.certs.p50twr}" 
+								class="cert cert_major" 
+								@click="toggleCert">Major Tower</span>
+							<span 
+								id="p50gnd" 
+								:class="{active: form.certs.p50gnd}" 
+								class="cert cert_major" 
+								@click="toggleCert">Major Ground</span>
+							<span 
+								id="app" 
+								:class="{active: form.certs.app}" 
+								class="cert cert_minor" 
+								@click="toggleCert">Minor Approach</span>
+							<span 
+								id="twr" 
+								:class="{active: form.certs.twr}" 
+								class="cert cert_minor" 
+								@click="toggleCert">Minor Tower</span>
+							<span 
+								id="gnd" 
+								:class="{active: form.certs.gnd}" 
+								class="cert cert_minor" 
+								@click="toggleCert">Minor Ground</span>
+						</div>
+					</div>
+					<div class="input-field col s12">
+						<label for="roles" class="active">Roles</label>
+						<div id="roles_container">
+							<span id="atm" class="cert cert_senior" :class="{active: form.roles.atm}" @click="toggleRole">ATM</span>
+							<span id="datm" class="cert cert_senior" :class="{active: form.roles.datm}" @click="toggleRole">DATM</span>
+							<span id="ta" class="cert cert_senior" :class="{active: form.roles.ta}" @click="toggleRole">TA</span>
+							<span id="ec" class="cert cert_junior" :class="{active: form.roles.ec}" @click="toggleRole">EC</span>
+							<span id="fe" class="cert cert_junior" :class="{active: form.roles.fe}" @click="toggleRole">FE</span>
+							<span id="wm" class="cert cert_junior" :class="{active: form.roles.wm}" @click="toggleRole">WM</span>
+							<span id="ins" class="cert cert_training" :class="{active: form.roles.ins}"  @click="toggleRole">INS</span>
+							<span id="mtr" class="cert cert_training" :class="{active: form.roles.mtr}"  @click="toggleRole">MTR</span>
+						</div>
+					</div>
+					<div class="input-field col s12">
+						<button type="submit" class="btn right">Update</button>
 					</div>
 				</div>
-				<div class="input-field col s12">
-					<label for="certs" class="active">Certifications</label>
-					<div id="certs_container">
-						<span class="cert cert_null" :class="{cert_center: certs.includes('zab')}" id="cert_center" @click="giveCert">Albuquerque Center</span>
-						<span class="cert cert_null" :class="{cert_major: certs.includes('p50app')}" id="cert_major" @click="giveCert">Major Approach</span>
-						<span class="cert cert_null" :class="{cert_major: certs.includes('p50twr')}" id="cert_major" @click="giveCert">Major Tower</span>
-						<span class="cert cert_null" :class="{cert_major: certs.includes('p50gnd')}" id="cert_major" @click="giveCert">Major Ground</span>
-						<span class="cert cert_null" :class="{cert_minor: certs.includes('app')}" id="cert_minor" @click="giveCert">Minor Approach</span>
-						<span class="cert cert_null" :class="{cert_minor: certs.includes('twr')}" id="cert_minor" @click="giveCert">Minor Tower</span>
-						<span class="cert cert_null" :class="{cert_minor: certs.includes('gnd')}" id="cert_minor"  @click="giveCert">Minor Ground</span>
-					</div>
-				</div>
-				<div class="input-field col s12">
-					<label for="roles" class="active">Roles</label>
-					<div id="roles_container">
-						<span class="cert cert_null" :class="{cert_senior: roles.includes('atm')}" id="cert_senior" @click="giveCert">ATM</span>
-						<span class="cert cert_null" :class="{cert_senior: roles.includes('datm')}" id="cert_senior" @click="giveCert">DATM</span>
-						<span class="cert cert_null" :class="{cert_senior: roles.includes('ta')}" id="cert_senior" @click="giveCert">TA</span>
-						<span class="cert cert_null" :class="{cert_junior: roles.includes('ec')}" id="cert_junior" @click="giveCert">EC</span>
-						<span class="cert cert_null" :class="{cert_junior: roles.includes('fe')}" id="cert_junior" @click="giveCert">FE</span>
-						<span class="cert cert_null" :class="{cert_junior: roles.includes('wm')}" id="cert_junior" @click="giveCert">WM</span>
-						<span class="cert cert_null" :class="{cert_training: roles.includes('ins')}" id="cert_training"  @click="giveCert">INS</span>
-						<span class="cert cert_null" :class="{cert_training: roles.includes('mtr')}" id="cert_training"  @click="giveCert">MTR</span>
-					</div>
-				</div>
-				<div class="input-field col s12">
-					<input type="submit" class="btn right" value="update" />
-				</div>
-			</div>
+			</form>
 		</div>
 	</div>
 </template>
 
 <script>
 import { ControllerMixin } from '@/mixins/ControllerMixin.js';
+import axios from '@/helpers/axios.js';
+
 export default {
 	data() {
 		return {
 			controller: null,
 			usedOi: [],
 			oi: "",
-			available: true,
-			certs: [],
-			roles: []
+			oiAvail: true,
+			form: {
+				fname: '',
+				lname: '',
+				email: '',
+				oi: '',
+				certs: {
+					zab: false,
+					p50app: false,
+					p50twr: false,
+					p50gnd: false,
+					app: false,
+					twr: false,
+					gnd: false
+				},
+				roles: {
+					atm: false,
+					datm: false,
+					ta: false,
+					ec: false,
+					fe: false,
+					wm: false,
+					ins: false,
+					mtr: false,
+				},
+			}
 		};
 	},
 	mixins: [ControllerMixin],
 	methods: {
 		async getController() {
 			this.controller = await this.getControllerMixin(this.$route.params.cid);
-			this.oi = this.controller.oi;
-			this.controller.certifications.forEach((cert) => { this.certs.push(cert.code)});
-			this.controller.roles.forEach((role) => { this.roles.push(role.code)});
-			this.usedOi = await this.getUsedOiMixin();
+			this.form = {
+				...this.form,
+				fname: this.controller.fname,
+				lname: this.controller.lname,
+				email: this.controller.email,
+				oi: this.controller.oi,
+			};
+			
+			this.controller.certifications.forEach(cert => this.form.certs[cert.code] = true);
+			this.controller.roles.forEach(role => this.form.roles[role.code] = true);
+			const oiObject = await this.getUsedOiMixin();
+			this.usedOi = oiObject.map(oi => oi.oi);
 		},
 		checkOi() {
-			if(this.oi == this.controller.oi) this.available = true;
-			else if(this.usedOi.some(oi => oi.oi === this.oi) || this.oi.length != 2) this.available = false;
-			else this.available = true;
+			this.oiAvail = (this.form.oi != this.controller.oi && (this.usedOi.includes(this.form.oi) || this.form.oi.length != 2)) ? false : true;
 		},
-		giveCert(e) {
-			var id = e.target.id;
-			e.target.classList.toggle(id);
+		toggleCert: function(e) {
+			e.target.classList.toggle('active');
+			this.form.certs[e.target.id] = e.target.classList.contains('active');
+		},
+		toggleRole: function(e) {
+			e.target.classList.toggle('active');
+			this.form.roles[e.target.id] = e.target.classList.contains('active');
+		},
+		updateController() {
+			const userToken = localStorage.getItem('token') || null;
+			axios.post(`/controller/${this.controller.cid}`, {
+				form: this.form
+			}, {
+				headers: { Authorization: `Bearer ${userToken}` }
+			}).then(() => {
+				
+			});
+
+			
 		}
 	},
 	mounted() {
@@ -120,41 +197,41 @@ export default {
 	margin: 2px;
 	cursor: pointer;
 	transition: background-color .3s ease;
+	background: $gray_mild;
+	color: $gray_medium;
 
-	&.cert_null {
-		background: $gray_mild;
-		color: #9e9e9e;
-	}
-
-	&.cert_senior {
-		background: $cert_senior!important;
-		color: #fff!important;
-	}
-
-	&.cert_junior {
-		background: $cert_junior!important;
-		color: #fff!important;
-	}
-
-	&.cert_training {
-		background: $cert_training!important;
-		color: #fff!important;
-	}
+	&.active {
+		&.cert_senior {
+			background: $cert_senior;
+			color: #fff;
+		}
 	
-	&.cert_center {
-		background-color: $secondary-color-dark!important;
-		color: #fff!important;
+		&.cert_junior {
+			background: $cert_junior;
+			color: #fff;
+		}
+	
+		&.cert_training {
+			background: $cert_training;
+			color: #fff;
+		}
+		
+		&.cert_center {
+			background-color: $secondary-color-dark;
+			color: #fff;
+		}
+	
+		&.cert_major {
+			background: $secondary-color;
+			color: #fff;
+		}
+	
+		&.cert_minor {
+			background: $secondary-color-light;
+			color: #fff;
+		}
 	}
 
-	&.cert_major {
-		background: $secondary-color!important;
-		color: #fff!important;
-	}
-
-	&.cert_minor {
-		background: $secondary-color-light!important;
-		color: #fff!important;
-	}
 }
 
 #certs_container, #roles_container {
