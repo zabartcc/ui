7d22a6f804a30756e909b912117830d71ba29335
Austin Robison <austin@arobison.net>
2021-02-03 19:41:20 -0700

    improve login system

--- src/views/page/Home.vue
+++ src/views/page/Home.vue
@@ -6,6 +6,12 @@
 				<p>
 					Hello, and welcome to the Albuquerque ARTCC. We are a group of aviation enthusiasts providing simulated ATC services, operating on the VATSIM network, the Americas region and the VATUSA division. The Albuquerque ARTCC covers most of Arizona and New Mexico as well as parts of Texas, Colorado and Oklahoma.
 				</p>
+				<div class="visit" v-if="!user.isLoggedIn">
+					<p>
+						If you would like to become a visitor, please click the button below.
+					</p>
+					<router-link to="/controllers/visit" class="btn btn-waves">Become a Visitor</router-link>
+				</div>
 			</div>
 		</div>
 		<NewsSummary />
@@ -14,16 +20,33 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import WeatherInfo from '../partial/WeatherInfo.vue';
 import NewsSummary from '../partial/NewsSummary.vue';
 export default {
 	components: {
 		NewsSummary,
 		WeatherInfo
+	},
+	computed: {
+		...mapState('user', [
+			'user'
+		])
 	}
 };
 </script>
 
 <style scoped lang="scss">
+.visit {
+	margin-top: 1em;
+	text-align: center;
 
+	p {
+		text-align: left;
+	}
+
+	.btn {
+		margin-top: 1em;
+	}
+}
 </style>
