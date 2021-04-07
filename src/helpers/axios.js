db1f0a830d63476933a222a147f76c0c8a55437b
Alexandra Robison <alexandra@arobison.net>
2021-04-06 20:17:26 -0700

    remove VATUSA API from test mode

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -9,14 +9,10 @@ export const vatusaApi = axios.create({
 	baseURL: 'https://api.vatusa.net/v2',
 	params: {
 		apikey: process.env.VUE_APP_VATUSA_API_KEY,
-		test: true
 	},
 });
 
 export const vatusaApiAuth = axios.create({
 	baseURL: 'https://api.vatusa.net/v2',
 	withCredentials: true,
-	params: {
-		test: true
-	},
 });
