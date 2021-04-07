9f25259ee7098d730909d4f310713c451a4711dc
Daan <daanj33@gmail.com>
2021-04-07 21:46:14 +0200

    Allow documents to be files

--- src/views/admin/files/documents/New.vue
+++ src/views/admin/files/documents/New.vue
@@ -22,9 +22,31 @@
 						<textarea id="description" class="materialize-textarea" data-length="400" v-model="form.description"></textarea>
 						<label for="description">Description (optional)</label>
 					</div>
-					<div class="col s12">
+					<div class="col s12 radio_select">
+						<span class="title">Type</span>
+						<p>
+							<label>
+								<input type="radio" value="file" v-model="type" />
+								<span>File</span>
+							</label>
+							<label>
+								<input type="radio" value="doc" v-model="type"/>
+								<span>Document</span>
+							</label>
+						</p>
+					</div>
+					<div class="col s12" v-show="type === 'doc'">
 						<span class="title">Content</span>
 						<div id="tui_editor"></div>
+					</div>
+					<div class="file-field input-field col s12" v-if="type === 'file'">
+						<div class="btn">
+							<span>FILE</span>
+							<input type="file" ref="download" id="fileInput" required>
+						</div>
+						<div class="file-path-wrapper">
+							<input class="file-path validate" type="text" placeholder="Upload a file">
+						</div>
 					</div>
 					<div class="input-field col s12">
 						<input type="submit" class="btn right" value="create" />
@@ -41,7 +63,7 @@ import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell'; // Merg
 import 'codemirror/lib/codemirror.css'; // Editor's Dependency Style
 import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
 import {zabApi} from '@/helpers/axios.js';
-
+import {mapState} from 'vuex';
 
 export default {
 	name: 'NewDoc',
@@ -55,6 +77,7 @@ export default {
 				content: ''
 			},
 			editor: null,
+			type: null
 		};
 	},
 	async mounted() {
@@ -74,17 +97,47 @@ export default {
 	},
 	methods: {
 		async addDocument() {
-			this.form.content = this.editor.getMarkdown();
-			const {data: addData} = await zabApi.post('/file/documents', this.form);
+			if(this.type === 'doc') {
+				this.form.content = this.editor.getMarkdown();
+				const {data: addData} = await zabApi.post('/file/documents', this.form);
 
-			if(addData.ret_det.code === 200) {
-				this.toastSuccess('Document successfully created');
+				if(addData.ret_det.code === 200) {
+					this.toastSuccess('Document successfully created');
 
-				this.$router.push('/admin/files/documents');
+					this.$router.push('/admin/files/documents');
+				} else {
+					this.toastError(addData.ret_det.message);
+				}
 			} else {
-				this.toastError(addData.ret_det.message);
+				this.toastInfo('Uploading...');
+				const formData = new FormData();
+				formData.append('name', this.form.name);
+				formData.append('category', this.form.category);
+				formData.append('description', this.form.description);
+				formData.append('download', this.$refs.download.files[0]);
+				formData.append('author', this.user.data._id);
+
+				const {data} = await zabApi.post(`/file/documents`, formData, {
+					headers: { 
+						'Content-Type': 'multipart/form-data'
+					}
+				});
+
+				if(data.ret_det.code === 200) {
+					this.toastSuccess('File successfully added');
+
+					document.getElementById("fileInput").value = "";
+					this.$router.push('/admin/files/documents');
+				} else {
+					this.toastError(data.ret_det.message);
+				}
 			}
 		}
+	},
+	computed: {
+		...mapState('user', [
+			'user'
+		]),
 	}
 };
 </script>
@@ -93,5 +146,13 @@ export default {
 .title {
 	color: #9E9E9E;
 	font-size: .75rem;
+}
+
+p label {
+	margin-right: 1.5em;
+}
+
+.radio_select {
+	margin-bottom: 1em;
 }
 </style>
--- src/views/files/DocumentCategory.vue
+++ src/views/files/DocumentCategory.vue
@@ -2,7 +2,8 @@
 	<div :id="cat" class="col s12">
 		<div v-if="docs.length === 0" class="no_files">No documents in this category found.</div>
 		<div class="document" v-else v-for="doc in docs" :key="doc.id">
-			<router-link :to="`/files/documents/${doc.slug}`" class="button btn"><i class="material-icons">search</i></router-link>
+			<router-link v-if="doc.type === 'doc'" :to="`/files/documents/${doc.slug}`" class="button btn"><i class="material-icons">search</i></router-link>
+			<a v-else :href="`https://zabartcc.sfo3.digitaloceanspaces.com/downloads/${doc.fileName}`" class="btn button"><i class="material-icons">search</i></a>
 			<div class="title">{{doc.name}}</div>
 			<div class="desc">{{doc.description}}</div>
 			<div class="info">Updated at {{dtRegionalUS(doc.updatedAt)}}z</div>
