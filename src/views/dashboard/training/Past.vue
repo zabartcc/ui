12e45734dc6f27993ad5add388c54884f639f311
Alexandra Robison <alexandra@arobison.net>
2021-04-09 15:39:26 -0700

    fix past training sessions

--- src/views/dashboard/training/Past.vue
+++ src/views/dashboard/training/Past.vue
@@ -22,7 +22,7 @@
 					<tr v-for="session in pastSessions" :key="session._id">
 						<td>{{dtLong(session.startTime)}}</td>
 						<td>{{dtLong(session.endTime)}}</td>
-						<td>{{session.milestone.name}}</td>
+						<td>{{(session.milestone) ? session.milestone.name : 'Unknown'}}</td>
 						<td>{{session.instructor ? session.instructor.fname + ' ' + session.instructor.lname : session.instructorCid}}</td>
 						<td class="options">
 							<router-link :to="`/dash/training/session/${session._id}`" data-position="top" data-tooltip="View Session Details" class="tooltipped">
