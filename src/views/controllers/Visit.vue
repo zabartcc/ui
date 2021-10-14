0292697e1f234238b62e10837c1225dc1174075a
Daan <daanj33@gmail.com>
2021-10-14 17:22:29 +0200

    Link to ZAB visiting controller policy instead of VATSIM's on visiting page

--- src/views/controllers/Visit.vue
+++ src/views/controllers/Visit.vue
@@ -3,7 +3,7 @@
 		<div class="card-content">
 			<span class="card-title">Become a Visitor</span>
 			<p v-if="!user.isLoggedIn">Thank you for your interest in visiting the Albuquerque ARTCC. To apply for visiting status, click the button below to login and continue.<br /></p>
-			<p><b class="red-text">Important: </b>please ensure that you are allowed to visit per <a href="https://www.vatsim.net/documents/transfer-and-visiting-controller-policy" target="_blank"><b>VATSIM's Transfer and Visiting Controller Policy</b></a>. Any application that doesn't meet the requirements as outlined in that policy will be rejected.</p>
+			<p><b class="red-text">Important: </b>please ensure that you meet all requirements to become a visitor, as outlined in our <router-link to="/files/documents/visiting-controller-policy-26476"><b>Visiting Controller Policy</b></router-link>. Any application that doesn't meet the requirements will be rejected.</p>
 			<div v-if="!user.isLoggedIn">
 				<button class="btn btn-waves login_button" @click="login">Login with VATSIM</button>
 			</div>
