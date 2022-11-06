c45928097e3c43a8ce4f1dc5b51c9989b8e4f67c
Cole D. Connelly <cdconn00@gmail.com>
2022-11-06 10:40:53 -0600

    Change redirect url to correct link.

--- src/helpers/uriHelper.js
+++ src/helpers/uriHelper.js
@@ -3,7 +3,7 @@ export const vatsimAuthRedirectUrl = `${
 }/authorize?client_id=${
   import.meta.env.VITE_VATSIM_AUTH_CLIENT_ID
 }&redirect_uri=${
-  window.location.origin
+  window.location.origin + "/login/verify"
 }&response_type=code&scope=vatsim_details`;
 
 export const discordRedirectUrl = `https://discord.com/api/oauth2/authorize?client_id=546531280842653696&redirect_uri=https%3A%2F%2Fzabartcc.org%2Fconnect%2Fdiscord&response_type=code&scope=identify`;
