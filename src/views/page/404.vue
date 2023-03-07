4eab95c8261b360c4bfaf6529df48504bf4213a4
Evan Bradley <36498248+abusivemilk@users.noreply.github.com>
2023-03-07 08:44:24 -0600

    Update 404.vue
    change to generic alias

--- src/views/page/404.vue
+++ src/views/page/404.vue
@@ -6,7 +6,7 @@
 				Cole and Gio made an uh-oh oopsie whoopsie uwu.
 			</p>
 			<p>
-				We're sorry, but the content you requested was not something we felt was important, or you typed something wrong. If you believe that you are receiving this message in error, please email the webmaster (giovanni.fatelli@zabartcc.org). In the meantime, a cute pupper has been dispatched to ease your sorrows.
+				We're sorry, but the content you requested was not something we felt was important, or you typed something wrong. If you believe that you are receiving this message in error, please email the webmaster (wm@zabartcc.org). In the meantime, a cute pupper has been dispatched to ease your sorrows.
 			</p>
 			<p>
 				<img src="@/assets/images/dog.jpg" alt="Cute dog">
