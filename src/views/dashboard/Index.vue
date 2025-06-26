fab3dea4460c9bac81ec11da64b5bb424974b522
Cole D. Connelly <cdconn00@gmail.com>
2025-06-26 08:51:32 -0500

    Add new verbiage for activity requirements.

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -1,261 +1,326 @@
 <template>
-	<div class="card home_intro">
-		<div class="card-content">
-			<span class="card-title">Controller Dashboard</span>
-			<div class="loading_container" v-if="!controllingSessions">
-				<Spinner />
-			</div>
-			<div v-else>
-				<div class="hours_info">
-					<span>
-						You have controlled for <b>{{ hoursCalc }}</b> in the past quarter.
-					</span>
-					<span v-if="user.data.rating !== 1">
-						You will need to control again by <b>{{ calcControlDate }}</b> to prevent removal from the roster.
-					</span>
-				</div>
-				<span class="section_title">
-					<br>IDS Token
-				</span>
-				<div class="hidden" id="token_wrap">
-					<code>{{ token }}</code>
-					<span class="generate right" @click="generateToken">
-						<i class="material-icons">refresh</i>
-					</span>
-					<div id="click_to_see" @click="showToken">Click to view</div>
-				</div>
-				<span class="section_title">
-					External Integrations
-				</span>
-				<div class="discord_connect">
-						<button class="btn waves-effect waves-light">
-							<a href="https://www.vatusa.net/my/profile" class="discord_text">Link Discord</a>
-						<img src="@/assets/images/discord.svg" alt="" draggable="false" class="discord_logo" height="24">	
-					</button>
-					<button class="btn-flat waves-effect">
-						<a href="https://discord.gg/zabartcc" target="_blank" rel="noreferrer noopener">Join Discord</a>
-					</button>
-				</div>	
-			</div>
-		</div>
-	</div>
-	<div class="card">
-		<div class="card-content">
-			<span class="card-title">Recent Connections</span>
-		</div>
-		<div class="loading_container" v-if="!controllingSessions">
-			<Spinner />
-		</div>
-		<p v-else-if="controllingSessions && !controllingSessions.length" class="no_sessions">There are no recent connections to display</p>
-		<div class="table_wrapper" v-else>
-			<table class="medium hover striped">
-				<thead>
-					<th>Position</th>
-					<th>Sign On</th>
-					<th>Sign Off</th>
-					<th>Length</th>
-				</thead>
-				<tbody>
-					<tr v-for="session in controllingSessions" :key="session.timeStart">
-						<td>{{ session.position }}</td>
-						<td>{{ dtLong(session.timeStart) }}</td>
-						<td>{{ dtLong(session.timeEnd) }}</td>
-						<td>{{ sec2hms((new Date(session.timeEnd).getTime() - new Date(session.timeStart).getTime()) / 1000) }}</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+  <div class="card home_intro">
+    <div class="card-content">
+      <span class="card-title">Controller Dashboard</span>
+      <div class="loading_container" v-if="!controllingSessions">
+        <Spinner />
+      </div>
+      <div v-else>
+        <div class="hours_info">
+          <span>
+            You have controlled for <b>{{ hoursCalc }}</b> in the past quarter.
+          </span>
+          <span v-if="user.data.rating !== 1 && !hasMetRequirement">
+            You must control a total of 180 minutes before
+            <b>{{ calcControlDate }}</b> to prevent removal from the roster.
+          </span>
+          <span v-else>
+            You've met the controlling requirement this quarter. Your next
+            activity window will start: <b>{{ calcControlDate }}</b
+            >.
+          </span>
+        </div>
+        <span class="section_title"> <br />IDS Token </span>
+        <div class="hidden" id="token_wrap">
+          <code>{{ token }}</code>
+          <span class="generate right" @click="generateToken">
+            <i class="material-icons">refresh</i>
+          </span>
+          <div id="click_to_see" @click="showToken">Click to view</div>
+        </div>
+        <span class="section_title"> External Integrations </span>
+        <div class="discord_connect">
+          <button class="btn waves-effect waves-light">
+            <a href="https://www.vatusa.net/my/profile" class="discord_text"
+              >Link Discord</a
+            >
+            <img
+              src="@/assets/images/discord.svg"
+              alt=""
+              draggable="false"
+              class="discord_logo"
+              height="24"
+            />
+          </button>
+          <button class="btn-flat waves-effect">
+            <a
+              href="https://discord.gg/zabartcc"
+              target="_blank"
+              rel="noreferrer noopener"
+              >Join Discord</a
+            >
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-content">
+      <span class="card-title">Recent Connections</span>
+    </div>
+    <div class="loading_container" v-if="!controllingSessions">
+      <Spinner />
+    </div>
+    <p
+      v-else-if="controllingSessions && !controllingSessions.length"
+      class="no_sessions"
+    >
+      There are no recent connections to display
+    </p>
+    <div class="table_wrapper" v-else>
+      <table class="medium hover striped">
+        <thead>
+          <th>Position</th>
+          <th>Sign On</th>
+          <th>Sign Off</th>
+          <th>Length</th>
+        </thead>
+        <tbody>
+          <tr v-for="session in controllingSessions" :key="session.timeStart">
+            <td>{{ session.position }}</td>
+            <td>{{ dtLong(session.timeStart) }}</td>
+            <td>{{ dtLong(session.timeEnd) }}</td>
+            <td>
+              {{
+                sec2hms(
+                  (new Date(session.timeEnd).getTime() -
+                    new Date(session.timeStart).getTime()) /
+                    1000
+                )
+              }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex';
-import { zabApi } from '@/helpers/axios.js';
-import { DateTime as luxon } from 'luxon';
+import { mapState, mapActions } from "vuex";
+import { zabApi } from "@/helpers/axios.js";
+import { DateTime as luxon } from "luxon";
 
 export default {
-	name: 'UserDash',
-	title: 'Dashboard',
-	data() {
-		return {
-			approvedAirports: ['ABQ', 'PHX', 'TUS', 'AMA', 'ELP', 'DMA', 'DVT', 'SDL', 'FFZ', 'CHD', 'IWA', 'GEU', 'GYR', 'LUF', 'PRC', 'FLG'],
-			token: '',
-			discordConnected: false,
-			controllingSessions: null,
-		};
-	},
-	async mounted() {
-		this.token = this.user.data.idsToken || 'None Set';
-		await this.getControllingSessions();
-	},
-	methods: {
-		async generateToken() {
-			const { data: tokenRet } = await zabApi.post('/user/idsToken');
-			if(tokenRet.ret_det.code === 200) {
-				this.toastSuccess('Token successfully generated');
-				this.token = tokenRet.data;
-			} else this.toastError(tokenRet.ret_det.message);
-		},
-		async getControllingSessions() {
-			const { data: sessionData } = await zabApi.get('/user/sessions');
-			this.controllingSessions = sessionData.data;
-		},
-		showToken() {
-			document.getElementById('token_wrap').classList.remove('hidden');
-			document.getElementById('click_to_see').classList.add('hidden');
-		},
-		hideToken() {
-			document.getElementById('token_wrap').classList.add('hidden');
-			document.getElementById('click_to_see').classList.remove('hidden');
-		},
-		sec2hms(secs){
-			const days = Math.floor(secs / 86400);
-			const remainderSeconds = secs % 86400;
-			const hms = new Date(remainderSeconds * 1000).toISOString().substring(11, 19);
-			return hms.replace(/^(\d+)/, h => `${+h + days * 24}`.padStart(2, '0'));
-		},
-		formatDate(value) {
-			const d = new Date(value);
-			return d.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' });
-		},
-		...mapActions('user', [
-			'getUser'
-		])
-	},
-	computed: {
-		...mapState('user', [
-			'user'
-		]),
-		hoursCalc() {
-			const now = luxon.utc();
-			const startOfQuarter = now.startOf('quarter');
-			let seconds = 0;
-			for(const session of this.controllingSessions) {
-				if(new Date(session.timeEnd) >= startOfQuarter.toJSDate()) {
-					const newSeconds = (new Date(session.timeEnd) - new Date(session.timeStart)) / 1000;
-					seconds += newSeconds;
-				}
-			}
-			return this.sec2hms(seconds);
-		},
-		calcControlDate() {
-			const now = luxon.utc();
-			const endOfQuarter = now.endOf('quarter');
-			return this.formatDate(endOfQuarter.toJSDate());
-		}
-	}
+  name: "UserDash",
+  title: "Dashboard",
+  data() {
+    return {
+      approvedAirports: [
+        "ABQ",
+        "PHX",
+        "TUS",
+        "AMA",
+        "ELP",
+        "DMA",
+        "DVT",
+        "SDL",
+        "FFZ",
+        "CHD",
+        "IWA",
+        "GEU",
+        "GYR",
+        "LUF",
+        "PRC",
+        "FLG",
+      ],
+      token: "",
+      discordConnected: false,
+      controllingSessions: null,
+    };
+  },
+  async mounted() {
+    this.token = this.user.data.idsToken || "None Set";
+    await this.getControllingSessions();
+  },
+  methods: {
+    async generateToken() {
+      const { data: tokenRet } = await zabApi.post("/user/idsToken");
+      if (tokenRet.ret_det.code === 200) {
+        this.toastSuccess("Token successfully generated");
+        this.token = tokenRet.data;
+      } else this.toastError(tokenRet.ret_det.message);
+    },
+    async getControllingSessions() {
+      const { data: sessionData } = await zabApi.get("/user/sessions");
+      this.controllingSessions = sessionData.data;
+    },
+    showToken() {
+      document.getElementById("token_wrap").classList.remove("hidden");
+      document.getElementById("click_to_see").classList.add("hidden");
+    },
+    hideToken() {
+      document.getElementById("token_wrap").classList.add("hidden");
+      document.getElementById("click_to_see").classList.remove("hidden");
+    },
+    sec2hms(secs) {
+      const days = Math.floor(secs / 86400);
+      const remainderSeconds = secs % 86400;
+      const hms = new Date(remainderSeconds * 1000)
+        .toISOString()
+        .substring(11, 19);
+      return hms.replace(/^(\d+)/, (h) => `${+h + days * 24}`.padStart(2, "0"));
+    },
+    formatDate(value) {
+      const d = new Date(value);
+      return d.toLocaleString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        timeZone: "UTC",
+      });
+    },
+    ...mapActions("user", ["getUser"]),
+  },
+  computed: {
+    ...mapState("user", ["user"]),
+    hoursCalc() {
+      const now = luxon.utc();
+      const startOfQuarter = now.startOf("quarter");
+      let seconds = 0;
+      for (const session of this.controllingSessions) {
+        if (new Date(session.timeEnd) >= startOfQuarter.toJSDate()) {
+          const newSeconds =
+            (new Date(session.timeEnd) - new Date(session.timeStart)) / 1000;
+          seconds += newSeconds;
+        }
+      }
+      return this.sec2hms(seconds);
+    },
+    calcControlDate() {
+      const now = luxon.utc();
+      const endOfQuarter = now.endOf("quarter");
+      return this.formatDate(endOfQuarter.toJSDate());
+    },
+    hasMetRequirement() {
+      const now = luxon.utc();
+      const startOfQuarter = now.startOf("quarter");
+      let seconds = 0;
+
+      for (const session of this.controllingSessions) {
+        if (new Date(session.timeEnd) >= startOfQuarter.toJSDate()) {
+          const newSeconds =
+            (new Date(session.timeEnd) - new Date(session.timeStart)) / 1000;
+          seconds += newSeconds;
+        }
+      }
+
+      return seconds / 60 >= 180;
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 #token_wrap {
-	background: $gray_light;
-	padding: 0.25em 0.5em 0.35em 0.5em;
-	border-radius: 2px;
-	height: 32px;
-	width: 100%;
-	max-width: 400px;
-	position: relative;
-	box-shadow: 0 1px 2px rgba(0, 0, 0, 0.30);
+  background: $gray_light;
+  padding: 0.25em 0.5em 0.35em 0.5em;
+  border-radius: 2px;
+  height: 32px;
+  width: 100%;
+  max-width: 400px;
+  position: relative;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
 
-	&.hidden {
-		code, .generate {
-			display: none;
+  &.hidden {
+    code,
+    .generate {
+      display: none;
 
-			i {
-				display: none;
-			}
-		}
-	}
-	code {
-		display: inline-block;
-		border-radius: 0;
-		padding-top: .1em;
-		white-space: nowrap;
-		overflow: auto;
-		width: 90%;
-	}
+      i {
+        display: none;
+      }
+    }
+  }
+  code {
+    display: inline-block;
+    border-radius: 0;
+    padding-top: 0.1em;
+    white-space: nowrap;
+    overflow: auto;
+    width: 90%;
+  }
 
-	.generate {
-		border-radius: 0;
-		width: 10%;
-		text-align: right;
+  .generate {
+    border-radius: 0;
+    width: 10%;
+    text-align: right;
 
-		i {
-			font-size: 18px;
-			margin-left: .25em;
-			margin-top: .20em;
-			vertical-align: top;
-			user-select: none;
-			cursor: pointer;
-		}
-	}
+    i {
+      font-size: 18px;
+      margin-left: 0.25em;
+      margin-top: 0.2em;
+      vertical-align: top;
+      user-select: none;
+      cursor: pointer;
+    }
+  }
 }
 
 .section_title {
-	user-select: none;
-	color: #9E9E9E;
-	font-size: .9rem;
-	display: inline-block;
+  user-select: none;
+  color: #9e9e9e;
+  font-size: 0.9rem;
+  display: inline-block;
 
-	&~.section_title {
-		margin-top: 2em;
-	}
+  & ~ .section_title {
+    margin-top: 2em;
+  }
 }
 
 #click_to_see {
-	position: absolute;
-	top: 0;
-	left: 0;
-	height: 100%;
-	width: 100%;
-	text-align: center;
-	cursor: pointer;
-	user-select: none;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-	&.hidden {
-		display: none;
-	}
+  &.hidden {
+    display: none;
+  }
 }
 
 .discord_title {
-	font-size: 0.85rem;
+  font-size: 0.85rem;
 
-	&.connect {
-		color: $accent-color;
-	}
+  &.connect {
+    color: $accent-color;
+  }
 }
 
 .discord_connect {
-	.btn {
-		padding-left: 2.5em;
-	}
+  .btn {
+    padding-left: 2.5em;
+  }
 
-	.discord_logo {
-		position: absolute;
-		left: 0;
-		margin-left: 6px;
-		margin-top: 7px;
-	}
-	
-	.discord_text{
-		color:azure
-	}
+  .discord_logo {
+    position: absolute;
+    left: 0;
+    margin-left: 6px;
+    margin-top: 7px;
+  }
+
+  .discord_text {
+    color: azure;
+  }
 }
 
 .table_wrapper {
-	overflow: auto;
+  overflow: auto;
 
-	table {
-		min-width: 100%;
-	}
+  table {
+    min-width: 100%;
+  }
 }
 
 .no_sessions {
-	padding: 0 1em 1em 1em;
-	margin-top: -1em;
-	font-style: italic;
+  padding: 0 1em 1em 1em;
+  margin-top: -1em;
+  font-style: italic;
 }
 </style>
